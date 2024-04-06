e2693e66c9af6da896ceba6f13da2e1ff9e42c7c
krezd <124604157+krezd@users.noreply.github.com>
2024-04-06 09:51:50 +0300

    3 задание

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,10 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как тебя зовут?");
-        Scanner in = new Scanner(System.in);
-        String name  = in.next();
-        System.out.println("Привет, " + name);
+        int[] array = new int[20];
+        Random rand = new Random();
+        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+        System.out.print("Массив: ");
+        for(int i = 0; i < 20;i++){
+            array[i] = rand.nextInt(1,16);
+            System.out.print(array[i]+ " ");
+            if(map.containsKey(array[i])){
+                int value = map.get(array[i]);
+                map.put(array[i], value+1);
+            }
+            else{
+                map.put(array[i],1);
+            }
+        }
+System.out.println();
+        for(Map.Entry<Integer,Integer> pair: map.entrySet()){
+            if(pair.getValue() > 1){
+                System.out.println("Число '" + pair.getKey() + "' встречается '" + pair.getValue() + "' раз");
+            }
+        }
+
+
     }
 }
